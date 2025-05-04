2df651e5b553fb822363aa225b8421b70b083a6d
Christopher Fossto <70951673+cfossto@users.noreply.github.com>
2025-05-04 22:44:10 +0200

    Update factory.py

--- factory/factory.py
+++ factory/factory.py
@@ -35,10 +35,16 @@ class ConcreteProduct1(Product):
   def operation(self) -> str:
     return "{Result of Product1}"
 
+class ConcreteProduct2(Product):
+  def operation(self) -> str:
+    return "{Result of Product2}"
+
 
 def client_code(creator: Creator) ->:
   print(f"Client: I am not aware of the creator's class, but it still works \n"
        f"{creator.some_operation()}")
 
 client_code(ConcreteProduct1)
-print("\n)
+print("\n")
+client_code(ConcreteProduct2)
+print("\n")
