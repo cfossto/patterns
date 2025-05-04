68c364fd18372926f40c3626d41042c29e7195a8
Christopher Fossto <70951673+cfossto@users.noreply.github.com>
2025-05-04 23:11:58 +0200

    Update factory.py

--- factory/factory.py
+++ factory/factory.py
@@ -2,49 +2,51 @@ from abc import ABC, abstractmethod
 
 class Creator(ABC):
 
-  @abstractmethod
-  def factory_method(self):
-    pass
+    @abstractmethod
+    def factory_method(self):
+        pass
 
-  def some_operation(self) -> str:
-    product = self.factory_method()
-    result = f"Creator: The same creator's code has just worked with {product.operation()}"
-
-    return result
-
-
-class ConcreteCreator1(Creator):
-
-  def factory_method(self):
-    return ConcreteProduct1
-
-class ConcreteCreator2(Creator):
-
-  def factory_method(self):
-    return ConcreteProduct2
+    def some_operation(self) -> str:
+        product = self.factory_method()
+        result = f"Creator: {product.operation()}"
+        return result
 
 
 class Product(ABC):
 
-  @abstractmethod
-  def operation(self) -> str:
-    pass
+    @abstractmethod
+    def operation(self) -> str:
+        pass
+
+
+class ConcreteCreator1(Creator):
+    def factory_method(self) -> Product:
+        return ConcreteProduct1()
+
+class ConcreteCreator2(Creator):
+    def factory_method(self) -> Product:
+        return ConcreteProduct2()
+
+
 
 
 class ConcreteProduct1(Product):
-  def operation(self) -> str:
-    return "{Result of Product1}"
+    def operation(self) -> str:
+        return "{Result of ConcreteProduct1}"
+
 
 class ConcreteProduct2(Product):
-  def operation(self) -> str:
-    return "{Result of Product2}"
+    def operation(self) -> str:
+        return "{Result of the ConcreteProduct2}"
 
 
-def client_code(creator: Creator) ->:
-  print(f"Client: I am not aware of the creator's class, but it still works \n"
-       f"{creator.some_operation()}")
+def client_code(creator: Creator) -> None:
+    print(f"{ creator.some_operation() }", end="")
 
-client_code(ConcreteProduct1)
-print("\n")
-client_code(ConcreteProduct2)
-print("\n")
+
+if __name__ == "__main__":
+    client_code(ConcreteCreator1())
+    print("\n")
+    client_code(ConcreteCreator2())
+
+    
