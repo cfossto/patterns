d69c6da1478eee9062b534c3eaf065b460dd8c06
Christopher Fossto <70951673+cfossto@users.noreply.github.com>
2025-05-04 20:39:35 +0200

    Update factory.java

--- factory/factory.java
+++ factory/factory.java
@@ -1,21 +1,53 @@
-public interface Shipment {
+// Example from GG
 
-  void ship();
-  void markAsArrived();
-  
+import java.io.*
+
+abstract class Vehicle {
+  public abstract void printVehicle();
 }
 
-public class AirShipment implements Shipment {
-
-  private bool shipped;
-  private bool arrived;
-  
-  public void ship(){
-    this.shipped = true;
+class TwoWheeler extends Vehicle {
+  public void printVehicle() {
+    System.out.println("I am a two wheeler!")
   }
+}
 
-  public void arrived(){
-    this.arrived = true;
-  }
+class FourWheeler extends Vehicle {
+  System.out.println("I am a four wheeler!")  
+}
+
+class Client {
   
+  private Vehicle pVehicle;
+
+  public Client(int type){
+    if (type == 1) {
+      pVehicle = new TwoWheeler();
+    }
+    if (type == 2) {
+      pVehicle = new FourWheeler();
+    }
+    else {
+      pVehicle = null;
+    }
+  }
+  public void cleanUp(){
+    if (pVehicle != null){
+      pVehicle = null;
+    }
+  }
+  public Vehicle getVehicle(){
+    return pVehicle;
+  }
+}
+
+public class M {
+  public static void Main(String[] args){
+    Client = new Client(1);
+    Vehicle pVehicle = client.getVehicle();
+    if (pVehicle != null){
+      pVehicle.printVehicle():
+    }
+    pVehicle.cleanUp();
+  }
 }
