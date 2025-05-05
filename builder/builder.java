0c0c6c8dc8512473b0819cf65233ccbafb04c2d5
Christopher Fossto <70951673+cfossto@users.noreply.github.com>
2025-05-05 23:17:27 +0200

    Create builder.java

---
+++ builder/builder.java
@@ -0,0 +1,51 @@
+public class Employee {
+
+  private final String name;
+  private final int id;
+
+  private final String department;
+  private final double salary;
+
+
+  // Private constructor
+  private Employee(Builder builder){
+    this.name = builder.name;
+    this.id = builder.id;
+    this.department = builder.department;
+    this.salary = builder.salary
+  }
+
+  public static class Builder {
+
+    private final String name;
+    private final int id;
+
+    private final String department;
+    private final double salary;
+
+    public Builder(String name, int id){
+      this.name = name;
+      this.id = id;
+    }
+
+    // Optional field to append to builder
+    public Builder department(string department){
+      this.department = department;
+      return this;
+    }
+
+    public Builder salary(double salary){
+      this.salary = salary;
+      return this;
+    }
+
+    public Employee build(){
+      return new Employee(this);
+    }
+    
+    @Override
+    public String toString() {
+        return "Employee{name='" + name + "', id=" + id +
+               ", department='" + department + "', salary=" + salary + '}'; 
+  }
+}
