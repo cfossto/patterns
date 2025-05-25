e00594d47613cb9307d56d129f0869b88ee55589
Christopher Fossto <70951673+cfossto@users.noreply.github.com>
2025-05-25 17:06:18 +0200

    Update notificationfactory.py

--- factory/notificationfactory.py
+++ factory/notificationfactory.py
@@ -1,24 +1,43 @@
+"""
+Simple factory for notifications. Goal is to make a factory that returns two methods of notification.
+Of course, the .notify() method is very simplistic, but here the factory is in focus. Not the method itself.
+
+Just know that this example does not have that much of other internals.. Just simplistic things here.
+I could have also added functionality, such as automatic notification on class creation.
+But that would be for another time and would also depend on the requests of the product owner.
+"""
+
 from abc import ABC, abstractmethod
 
 class Notification(ABC):
+  """
+  All notifications should have at least a .notify() method. This ABC makes sure it is so.
+  """
+  
   @abstractmethod
   def notify(self):
     pass
 
 
 class SMSNotification(Notification):
-
+  """This is a SMS notification class. It handles SMS notifications. Well. Pseudosms."""
+  
   def notify(self):
     print("SMS notification")
     return "{SMS notification}"
 
 class EmailNotification(Notification):
+  """This mocks a Email notification class."""
 
   def notify(self):
     print("Email notification")
     return "{SMS notification}"
 
-def notificationFactory(method):
+def notificationFactory(method: str):
+  """
+  Factory returns notifications based on string input.
+  """
+  
   if method == "email":
     return EmailNotification()
   elif method == "sms":
@@ -29,8 +48,11 @@ def notificationFactory(method):
 
 if __name__ == "__main__":
 
-    notification = notificationFactory("email")
-    notification.notify()
+    # Create an email-notification.
+    email_notification = notificationFactory("email")
+    email_notification.notify()
+    
+    # Create an SMS notification.
     n2 = notificationFactory("sms")
     n2.notify()
 
